e85a9df05d95cd8fe4e721b283b7d0ef12325d29
xd <107454339+bwyatt9@users.noreply.github.com>
2025-05-18 22:12:58 -0400

    Update fromDB.js

--- fromDB.js
+++ fromDB.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
+
 const app = express();
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
+
 app.get("/", async (req, res) => {
   try {
     const { data, error } = await supabase.from("matches").select("*");
     if (error) {
-      console.error("Read error:", error);
-      return res.status(500).json({ error: " error" });
+      console.log("couldn't read from db", error);
+      return res.status(500).json({ error: "problem" });
     }
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "error" });
+    res.status(500).json({ error: "something broke" });
   }
 });
 
